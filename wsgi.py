d56f7e770b6c417f2ffb203243d827369aae3ca1
frozyaka <evgenit321@gmail.com>
2025-01-12 22:49:38 +0300

    new
    new

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from api import create_app, emit_game_updates
+import config as config
+
+app, socketio = create_app(config.ProductionConfig)
+
+if __name__ == '__main__':
+    try:
+        socketio.run(app, host='0.0.0.0', port=5000)
+    except KeyboardInterrupt:
+        print('Exit')
